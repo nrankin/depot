5057078124257df75a611985a69f2ab0dd995ac2
Nicola Rankin <nicola.rankin@gmail.com>
2015-03-02 19:49:43 +1300

    make the listing page look nicer

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,9 @@ Product.delete_all
 
 Product.create!(title: 'Programming Ruby 1.9 & 2.0',
   description:
-    %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
+    %{<p>  Ruby is the fastest growing and most exciting dynamic language
         out there. If you need to get working programs deliverd fast,
-        you should add Ruby to your toolbox.
-    </p>},
+        you should add Ruby to your toolbox.  </p>
+    },
   image_url: 'ruby.jpg',
   price: 49.95)
